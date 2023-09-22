b208fcb4cad9a88f86d545fa7ebf413722e2dcd5
Anna Bolotova <anechkamilaha69@gmail.com>
2023-09-22 20:59:04 +0300

    создана вторая функция для добавления заметки в журнал

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 def create_note_log (file):
     with open(file, 'w', encoding ='utf-8') as f:
         note_log = []
-        json.dump(note_log, f, indent=2, ensure_ascii=False)   
+        json.dump(note_log, f, indent=2, ensure_ascii=False)  
+def add_note (file):
+    id = input("введите идентификатор заметки: ")
+    title = input("введите заголовок заметки: ")
+    core = input("введите тело заметки: ")
+    from datetime import datetime
+    date_of_creation = datetime.now().date()
+    new_date_of_creation = str(date_of_creation)
+    note= {
+        'id' : id, 
+        'title' : title,
+        'core' : core,
+        'date_of_creation' : new_date_of_creation
+        }
+    data = json.load(open(file))
+    data.append(note)
+    with open(file, "w", encoding='utf8') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+ 
 work_file = 'notes.json'
 import json
 print("Для работы с программой введите пожлуйста одну из следующих цифр:\n"
@@ -14,3 +32,6 @@ print("Для работы с программой введите пожлуйс
 comand = int(input("цифра: "))
 if(comand==1):
     create_note_log(work_file)
+if(comand==2):
+    add_note(work_file)
+
